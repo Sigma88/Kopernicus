87dea69e8c71c361b6076d5055f84b49fc8aa24d
R-T-B <21GunSoftware@comcast.net>
2020-08-12 18:01:18 -0700

    Mother of all Try-Catches...

--- src/Kopernicus/RuntimeUtility/RuntimeUtility.cs
+++ src/Kopernicus/RuntimeUtility/RuntimeUtility.cs
@@ -166,32 +166,40 @@ namespace Kopernicus.RuntimeUtility
             FixCameras();
             PatchTimeOfDayAnimation();
             StartCoroutine(CallbackUtil.DelayedCallback(3, FixFlags));
-
-            for (Int32 i = 0; i < PSystemManager.Instance.localBodies.Count; i++)
-            {
-                PatchStarReferences(PSystemManager.Instance.localBodies[i]);
-                PatchContractWeight(PSystemManager.Instance.localBodies[i]);
-            }
             //Small Contract fixer to remove Sentinel Contracts:
             Type contractTypeToRemove = null;
-            foreach (Type contract in Contracts.ContractSystem.ContractTypes)
-            {
-                if (contract.FullName.Contains("SentinelContract"))
-                {
-                    contractTypeToRemove = contract;
-                }
-            }
             try
             {
-                if (!(contractTypeToRemove == null))
+                foreach (Type contract in Contracts.ContractSystem.ContractTypes)
                 {
-                    ContractSystem.ContractTypes.Remove(contractTypeToRemove);
-                    contractTypeToRemove = null;
+                    try
+                    {
+
+                        if (contract.FullName.Contains("SentinelContract"))
+                        {
+                            contractTypeToRemove = contract;
+                        }
+                    }
+                    catch
+                    {
+                        continue;
+                    }
                 }
+            if (!(contractTypeToRemove == null))
+            {
+                ContractSystem.ContractTypes.Remove(contractTypeToRemove);
+                contractTypeToRemove = null;
+            }
             }
             catch
             {
                 contractTypeToRemove = null;
+            }
+            //Patch weights of contracts
+            for (Int32 i = 0; i < PSystemManager.Instance.localBodies.Count; i++)
+            {
+                PatchStarReferences(PSystemManager.Instance.localBodies[i]);
+                PatchContractWeight(PSystemManager.Instance.localBodies[i]);
             }
         }
 
