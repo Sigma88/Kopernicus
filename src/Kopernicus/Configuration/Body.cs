d2a200f9c95a225a68c3b61c026d4879a46d811e
R-T-B <21GunSoftware@comcast.net>
2020-09-03 16:06:07 -0700

    Quick fix for 1.9.1.

--- src/Kopernicus/Configuration/Body.cs
+++ src/Kopernicus/Configuration/Body.cs
@@ -291,7 +291,7 @@ namespace Kopernicus.Configuration
                 {
                     if ((!Name.Equals("Jool")) || (Name.Equals("Jool") && (Template.Body.celestialBody.Radius > 6000000))) // This is a Jool-clone, or resized Jool.  We have to handle it special.
                     {
-                        try
+                        /*try
                         {
                             //Remove Gas Giant shaders for compatability
                             GasGiantMaterialControls GGMC = GeneratedBody.scaledVersion.GetComponent<GasGiantMaterialControls>();
@@ -302,7 +302,7 @@ namespace Kopernicus.Configuration
                         catch
                         {
                             //we don't care that it did not work, probably 1.9.1
-                        }
+                        }*/
                     }
                 }
                 // Create accessors
@@ -352,7 +352,7 @@ namespace Kopernicus.Configuration
             }
             // Event
             Events.OnBodyApply.Fire(this, node);
-        } // ^^^ Everything up there needs cleanup, highly redundant code.
+        }
 
         // Parser Post Apply Event
         void IParserEventSubscriber.PostApply(ConfigNode node)
